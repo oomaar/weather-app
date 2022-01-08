ca776cb6ac9d8413370736448ddb4b30f1ef9e8a
Omar <omaar_5@hotmail.com>
2022-01-08 16:14:49 +0200

    setup working baseURL with a valid API KEY

--- website/app.js
+++ website/app.js
@@ -1,4 +1,7 @@
 /* Global Variables */
+const apiKey = `584befef1f6116016438014a9b30ff2a`;
+const zipCode = document.getElementById("zip").value;
+const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=imperial`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
