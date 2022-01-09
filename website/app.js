70ae99351e7213ebf72b2011a66d234d0d76f44a
Omar <omaar_5@hotmail.com>
2022-01-09 14:39:03 +0200

    final app version

--- website/app.js
+++ website/app.js
@@ -1,8 +1,28 @@
-/* Global Variables */
-const apiKey = `584befef1f6116016438014a9b30ff2a`;
-const zipCode = document.getElementById("zip").value;
-const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=imperial`;
+const feeling = document.getElementById("feelings");
+const zipCode = document.getElementById("zip");
+const apiKey = "584befef1f6116016438014a9b30ff2a";
 
-// Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", async () => {
+    const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}&appid=${apiKey}&units=imperial`;
+    const res = await fetch(baseURL).then(data => data.json());
+
+    await fetch("/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            temp: res.main.temp,
+            date: newDate,
+            feeling: feeling.value
+        })
+    });
+
+    const allData = await fetch("/all").then(data => data.json());
+    document.getElementById("temp").innerHTML = `Temp: ${allData.temp} &deg;C`;
+    document.getElementById("date").innerHTML = `Date: ${allData.date}`;
+    document.getElementById("content").innerHTML = `Feeling: ${allData.feeling}`;
+});
